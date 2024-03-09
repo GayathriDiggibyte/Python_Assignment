2a15a4db3dbb09637c0c8c85b676c7c2a6cc382f
GayathriDiggibyte <gayathri.m@diggibyte.com>
2024-03-09 18:08:31 +0530

    Assignment4

---
+++ src/Assignment4/driver.py
@@ -0,0 +1,6 @@
+from src.Assignment4.util import merge_the_tools
+def main():
+    s=input("Enter a string : ")
+    k=int(input("Enter the key : "))
+    merge_the_tools(s,k)
+main()
---
+++ src/Assignment4/util.py
@@ -0,0 +1,21 @@
+def merge_the_tools(string, k):
+    # your code goes here
+    n=len(string)
+    parts=k
+    p=parts
+    string_parts=[]
+    for i in range(0,n,p):
+        s=string[i:p]
+        p=p+parts
+        string_parts.append(s)
+    print(string_parts)
+    res=[]
+    for x in string_parts:
+        unique_parts = ""
+        for y in x:
+            if y not in unique_parts:
+                unique_parts = unique_parts + y
+        res.append(unique_parts)
+
+    for e in res:
+        print(e)
