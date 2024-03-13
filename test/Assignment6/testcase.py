48e081e8b1ec0ead79d3d4bdbe2a8629dcd19687
GayathriDiggibyte <gayathri.m@diggibyte.com>
2024-03-13 17:17:06 +0530

    Assignment 6 testcase

--- test/Assignment6/testcase.py
+++ test/Assignment6/testcase.py
@@ -1,8 +1,28 @@
 import unittest
+from src.Assignment6.util import print_pattern
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+    def testcase1(self):
+        expected_output = [
+            '  H  ',
+            ' HHH ',
+            'HHHHH',
+            ' HHH         HHH        ',
+            ' HHH         HHH        ',
+            ' HHH         HHH        ',
+            ' HHH         HHH        ',
+            ' HHHHHHHHHHHHHHH  ',
+            ' HHHHHHHHHHHHHHH  ',
+            ' HHH         HHH        ',
+            ' HHH         HHH        ',
+            ' HHH         HHH        ',
+            ' HHH         HHH        ',
+            '            HHHHH ',
+            '             HHH  ',
+            '              H   ',
+        ]
+        self.assertEqual(expected_output, print_pattern(3))  # add assertion here
+
 
 if __name__ == '__main__':
     unittest.main()
