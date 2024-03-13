d62186fc83d5e1cf173646267d69deea6d60380b
GayathriDiggibyte <gayathri.m@diggibyte.com>
2024-03-13 15:54:22 +0530

    Assignment 6

---
+++ src/Assignment6/driver.py
@@ -0,0 +1,4 @@
+from src.Assignment6.util import print_pattern
+# Take user input for the thickness
+thickness = int(input("Enter thickness (must be an odd number): "))
+print(print_pattern(thickness))
